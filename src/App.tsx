f67846ec5182c71012cf706961abcf8bb89e6599
Gustavo <avoguga@gmail.com>
2023-05-23 17:19:09 -0300

    Configurando firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "./firebase";
 
 function App() {
   const [fileUrl, setFileUrl] = useState<string | null>(null);
+  const [fileName, setFileName] = useState<string | null>(null);
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.item(0);
     if (file) {
-      setFileUrl(URL.createObjectURL(file));
+      const storage = getStorage(app);
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (error) => {
+          console.error("Upload failed:", error);
+        },
+        async () => {
+          const fileUrl = await getDownloadURL(uploadTask.snapshot.ref);
+          setFileUrl(fileUrl);
+          setFileName(file.name);
+        }
+      );
     }
   };
 
@@ -15,18 +44,24 @@ function App() {
     <div className="container">
       <h1>Upload and view PDF</h1>
       <div>
-        <input type="file" id="fileInput" onChange={handleFileChange} accept=".pdf" />
+        <input
+          type="file"
+          id="fileInput"
+          onChange={handleFileChange}
+          accept=".pdf"
+        />
         <label htmlFor="fileInput">Choose a PDF</label>
       </div>
-      {fileUrl && 
+      {fileUrl && (
         <div>
           <a href={fileUrl} target="_blank" rel="noopener noreferrer">
             View PDF
           </a>
+          <p className="file-name">{fileName}</p>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage, ref } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfLIwKYBxj1HM_R79BnvIctzwMwEmdV4s",
+  authDomain: "pdf-link-converter.firebaseapp.com",
+  projectId: "pdf-link-converter",
+  storageBucket: "pdf-link-converter.appspot.com",
+  messagingSenderId: "739777383178",
+  appId: "1:739777383178:web:7ce21494a439c675f908f3",
+  measurementId: "G-0QKHDWQGHP",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
